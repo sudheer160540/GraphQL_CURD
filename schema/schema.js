f608ea2bb9f7ff1fad7fa4b62f1555504fc00d98
Sudheer Nunna <sudheer.n@imimobile.com>
2020-01-29 14:37:56 +0530

    init commit

---
+++ schema/schema.js
@@ -0,0 +1,147 @@
+const graphql = require('graphql');
+const Book = require('../models/book');
+const Author = require('../models/Author');
+const User = require('../models/user');
+
+const { 
+    GraphQLObjectType, GraphQLString, 
+    GraphQLID, GraphQLInt,GraphQLSchema, 
+    GraphQLList,GraphQLNonNull 
+} = graphql;
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    //We are wrapping fields in the function as we dont want to execute this ultil 
+    //everything is inilized. For example below code will throw error AuthorType not 
+    //found if not wrapped in a function
+    fields: () => ({
+        id: { type: GraphQLID  },
+        name: { type: GraphQLString }, 
+        pages: { type: GraphQLInt },
+        author: {
+        type: AuthorType,
+        resolve(parent, args) {
+            return Author.findById(parent.authorID);
+        }
+    }
+    })
+});
+
+const userType = new GraphQLObjectType({
+     name:'User',
+     fields:()=>({
+         id:{type:GraphQLID},
+         name:{type:GraphQLString},
+         email:{type:GraphQLString}
+     })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        book:{
+            type: new GraphQLList(BookType),
+            resolve(parent,args){
+                return Book.find({ authorID: parent.id });
+            }
+        }
+    })
+})
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            //argument passed by the user while making the query
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                //Here we define how to get data from database source
+
+                //this will return the book with id passed in argument 
+                //by the user
+                return Book.findById(args.id);
+            }
+        },
+        books:{
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return Book.find({});
+            }
+        },
+        author:{
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Author.findById(args.id);
+            }
+        },
+        authors:{
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args) {
+                return Author.find({});
+            }
+        },
+        user:{
+           type :new GraphQLList(userType),
+           resolve(parents,args){
+               return User.find({});
+           }
+        }
+    }
+});
+
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                //GraphQLNonNull make these field required
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) }
+            },
+            resolve(parent, args) {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                });
+                return author.save();
+            }
+        },
+        addBook:{
+            type:BookType,
+            args:{
+                name: { type: new GraphQLNonNull(GraphQLString)},
+                pages: { type: new GraphQLNonNull(GraphQLInt)},
+                authorID: { type: new GraphQLNonNull(GraphQLID)}
+            },
+            resolve(parent,args){
+                let book = new Book({
+                    name:args.name,
+                    pages:args.pages,
+                    authorID:args.authorID
+                })
+                return book.save()
+            }
+        },
+        addUser:{
+            type:userType,
+            args:{
+                name:{ type: new GraphQLNonNull(GraphQLString)},
+                email:{ type: new GraphQLNonNull(GraphQLString)},
+            },resolve(parent,args){
+                let user = new User({
+                    name:args.name,
+                    email:args.email
+                })
+                return user.save();
+            }
+        }
+    }
+});
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+    mutation:Mutation
+});
